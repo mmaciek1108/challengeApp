2a5d4dea58318d4f4039841f463737313e60ce71
Maciej Marczydło <marczydlo@wp.pl>
2023-11-19 13:45:25 +0100

    zadanie z dnia 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Maciej";
+char sex = 'm';
+int age = 70;
+
+if ( sex  == 'k' && age < 30 )
+    {
+        Console.WriteLine ("Kobieta poniżej 30 lat ");
+    }
+else if (name == "Ewa" && age == 33)
+    {
+        Console.WriteLine("Ewa ,lat 33 ");
+    }
+else if ( sex == 'm' && age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyżna o imieniu "+ name);
+    }
+else
+    {
+        Console.WriteLine("ktos inny, osoba o imieniu "+ name + " i wieku " +age);
+    }
